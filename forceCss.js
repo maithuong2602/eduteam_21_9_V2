90ee7c5d9698315ba1449ff05029a517e37b5eb0
maithuong2602 <lethimaithuong@gmail.com>
2026-09-21 12:12:43 +0700

    Force CSS

---
+++ forceCss.js
@@ -0,0 +1,14 @@
+﻿const fs = require('fs');
+let content = fs.readFileSync('src/app/globals.css', 'utf8');
+content += `
+
+/* Vô hiệu hóa mọi làm mờ của trình duyệt */
+select, option, input {
+  color: #000 !important;
+  opacity: 1 !important;
+  font-weight: 800 !important;
+  -webkit-text-fill-color: #000 !important;
+}
+`;
+fs.writeFileSync('src/app/globals.css', content);
+console.log('Added global override');
